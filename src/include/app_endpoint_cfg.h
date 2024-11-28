7a4b3f004d3773fbbfc602e30b3f91d07917f803
Fedor <lfa1@yandex.ru>
2024-11-28 20:32:05 +0300

    Update app_endpoint_cfg.h

--- src/include/app_endpoint_cfg.h
+++ src/include/app_endpoint_cfg.h
@@ -72,8 +72,10 @@ typedef struct {
  */
 typedef struct {
     int16_t     localTemperature;               // 22°C * 100
-    int16_t     absMinHeatSetpointLimit;        // min 0°C * 100
-    int16_t     absMaxHeatSetpointLimit;        // max 50°C * 100
+    int16_t     absMinHeatSetpointLimit;        // min 5°C * 100
+    int16_t     absMaxHeatSetpointLimit;        // max 45°C * 100
+    int16_t     minHeatSetpointLimit;           // min 5°C * 100
+    int16_t     maxHeatSetpointLimit;           // max 45°C * 100
     int8_t      localTemperatureCalibration;    // in device -9°C...+9°C
     int16_t     occupiedHeatingSetpoint;        // 0°C ... 50°C * 100
     uint8_t     controlSequenceOfOperation;     // 0x02 Heating
@@ -83,9 +85,10 @@ typedef struct {
     uint8_t     weeklyTransNum;
     uint8_t     dailyTransNum;
     uint8_t     manual_progMode;                // 0x00 - manual mode, 0x01 - programming mode
-    uint8_t     temperatureDisplayMode;         // 0x00 - °C, 0x01 - °F. Always °C
-    uint8_t     keypadLockout;                  // on off
     uint8_t     sensor_used;                    // IN, ALL, OU - castom attribute
+    uint8_t     hysteresis;                     // 1 ... 5 °C
+    uint8_t     temperatureDisplayMode;         // 0x00 - °C, 0x01 - °F. Always °C (Not support)
+    uint8_t     keypadLockout;                  // on off
 } zcl_thermostatAttr_t;
 
 
