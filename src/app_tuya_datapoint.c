8ee6b9ecba995032eb04db32e7816e3dce7febec
Fedor <lfa1@yandex.ru>
2024-11-30 20:24:02 +0300

    Update app_tuya_datapoint.c

--- src/app_tuya_datapoint.c
+++ src/app_tuya_datapoint.c
@@ -6,7 +6,7 @@
  * "u9bfwha0"
  *
 */
-data_point_st_t data_point_type0[DP_IDX_MAXNUM] = {
+data_point_st_t data_point_model0[DP_IDX_MAXNUM] = {
         {DP_TYPE0_ID_01, DP_BOOL, 1,    1},             // on off
         {DP_TYPE0_ID_18, DP_VAL,  4,    10},            // local temperature
         {DP_TYPE0_ID_10, DP_VAL,  4,    1},             // heat setpoint
@@ -14,7 +14,7 @@ data_point_st_t data_point_type0[DP_IDX_MAXNUM] = {
         {DP_TYPE0_ID_13, DP_VAL,  4,    1},             // max heat setpoint
         {DP_TYPE0_ID_1A, DP_VAL,  4,    1},             // hysteresis
         {DP_TYPE0_ID_1B, DP_VAL,  4,    1},             // local temperature calibration
-        {DP_TYPE0_ID_24, DP_BOOL, 1,    1},             // 0x00 - heat, 0x01 - idle
+        {DP_TYPE0_ID_24, DP_ENUM, 1,    1},             // 0x00 - heat, 0x01 - idle
         {DP_TYPE0_ID_2B, DP_ENUM, 1,    1},             // sensor IN/AL/OU
         {DP_TYPE0_ID_02, DP_ENUM, 1,    1},             // manual (setpoint) / programming (schedule)
         {DP_TYPE0_ID_28, DP_BOOL, 1,    1},             // lock / unlock keys (child lock)
@@ -22,7 +22,7 @@ data_point_st_t data_point_type0[DP_IDX_MAXNUM] = {
         {DP_TYPE0_ID_03, DP_BOOL, 1,    1},             // unknown
 };
 
-data_point_st_t data_point_type1[DP_IDX_MAXNUM] = {{0}};
+data_point_st_t data_point_model1[DP_IDX_MAXNUM] = {{0}};
 
 static const char8_t *tuya_manuf_name0[] = {
 //                                           "aoclfnxz",
@@ -39,7 +39,21 @@ static const char8_t *tuya_manuf_name1[] = {
                                             NULL};
 
 
-data_point_st_t *data_point_type = data_point_type0;
 const char8_t **tuya_manuf_names[] = {tuya_manuf_name0, tuya_manuf_name1};
 uint8_t manuf_name = MANUF_NAME_0;
 
+data_point_st_t *data_point_model_arr[] = {data_point_model0, data_point_model1};
+
+// Not worked in gcc 4.5.1
+//data_point_st_t *data_point_model = data_point_model_arr[MANUF_NAME_0];
+data_point_st_t *data_point_model = data_point_model0;
+
+void data_point_model_init() {
+
+    data_point_model = data_point_model_arr[MANUF_NAME_0];
+
+//    for(uint16_t i = 0; i < DP_IDX_MAXNUM; i++) {
+//        printf("i: %d, id: 0x%x, type: %d, len: %d, divisor: %d\r\n",
+//                i, data_point_model[i].id, data_point_model[i].type, data_point_model[i].len, data_point_model[i].divisor);
+//    }
+}
