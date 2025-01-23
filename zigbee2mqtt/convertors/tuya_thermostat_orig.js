574f660597590b9676d7a7afad2c65ce438e4210
noidkn <noid@engineer.com>
2025-01-23 18:04:27 +0300

    Update tuya_thermostat_orig.js
    add  _TZE200_edl8pz1k

--- zigbee2mqtt/convertors/tuya_thermostat_orig.js
+++ zigbee2mqtt/convertors/tuya_thermostat_orig.js
@@ -1,7 +1,8 @@
 const {identify, reporting, ota} = require('zigbee-herdsman-converters/lib/modernExtend');
 
 const definition = {
-    fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE204_u9bfwha0'},
+    fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_edl8pz1k'},
+                  {modelID: 'TS0601', manufacturerName: '_TZE204_u9bfwha0'},
                   {modelID: 'TS0601', manufacturerName: '_TZE204_aoclfnxz'},
                   {modelID: 'TS0601', manufacturerName: '_TZE204_edl8pz1k'},
                   {modelID: 'TS0601', manufacturerName: '_TZE204_tagezcph'},
