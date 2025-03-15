6ea9bae19c8acb075253f99ffcad9c0efb2ad186
Fedor <lfa1@yandex.ru>
2025-03-15 20:46:08 +0300

    manufacturer code replacement

--- src/bootLoader/version_cfg.h
+++ src/bootLoader/version_cfg.h
@@ -50,7 +50,9 @@
  * During OTA upgrade, the upgraded device will check the rules of the following three fields.
  * Refer to ZCL OTA specification for details.
  */
-#define MANUFACTURER_CODE_TELINK           	0x1141//Telink ID
+#define SLACKY_MANUF_CODE                   0x6565
+#define MANUFACTURER_CODE_TELINK            SLACKY_MANUF_CODE//Custom ID
+//#define MANUFACTURER_CODE_TELINK           	0x1141//Telink ID
 #define	IMAGE_TYPE							((CHIP_TYPE << 8) | IMAGE_TYPE_BOOTLOADER)
 #define	FILE_VERSION					  	((APP_RELEASE << 24) | (APP_BUILD << 16) | (STACK_RELEASE << 8) | STACK_BUILD)
 
