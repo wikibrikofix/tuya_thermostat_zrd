6f57d4baeaa8eedfad106b96ea2cd7d0ef60e855
Fedor <lfa1@yandex.ru>
2025-03-17 22:45:56 +0300

    additional bootloader check and erase of the OTA area after the update

--- src/app_bootloader.c
+++ src/app_bootloader.c
@@ -1,10 +1,12 @@
 #include "app_main.h"
 
+#define BUFF_SIZE   256
+
 void bootloader_check() {
 
     uint8_t  marker[11] = BOOTLOAD_MARKER;
     uint8_t  buff[10] = {0};
-    uint8_t  bootloader_buff[256] = {0};
+    uint8_t  bootloader_buff[BUFF_SIZE] = {0};
     uint32_t bootloader_offset = 0;
     uint32_t bootloader_addr = BOOTLOADER_ADDR;
     uint32_t image_ota_addr = IMAGE_OTA_ADDR1;
@@ -73,22 +75,34 @@ void bootloader_check() {
 
     bootloader_offset = image_ota_addr + fw_header.bin_size;
 
-    for (uint32_t i = 0; i < boot_header.bin_size; i += 256) {
+    for (uint32_t i = 0; i < boot_header.bin_size; i += BUFF_SIZE) {
         if (i % FLASH_SECTOR_SIZE == 0) {
 //            printf("erase_addr: 0x%x\r\n", i);
             flash_erase(bootloader_addr + i);
         }
 //        printf("read_addr: 0x%x\r\n", bootloader_offset+i);
-        flash_read_page(bootloader_offset+i, 256, bootloader_buff);
+        flash_read_page(bootloader_offset+i, BUFF_SIZE, bootloader_buff);
 //        printf("write_addr: 0x%x\r\n", bootloader_addr+i);
-        flash_write(bootloader_addr+i, 256, bootloader_buff);
+        flash_write(bootloader_addr+i, BUFF_SIZE, bootloader_buff);
 
     }
 
-    flash_erase(BOOTLOAD_MARKER_SECTOR);
-    flash_read(0, 256, bootloader_buff);
+    for (uint8_t i = 0; i < 10; i++) {
+        flash_erase(BOOTLOAD_MARKER_SECTOR);
+        flash_read(0, BUFF_SIZE, bootloader_buff);
 
-    flash_write(BOOTLOAD_MARKER_ADDR, marker[0], marker+1);
+        flash_write(BOOTLOAD_MARKER_ADDR, marker[0], marker+1);
+
+        memset(bootloader_buff, 0, BUFF_SIZE);
+
+        flash_read_page(BOOTLOAD_MARKER_ADDR, marker[0], buff);
+
+        if (memcmp(buff, marker+1, marker[0]) == 0) {
+//            printf("i: %d\r\n", i);
+            break;
+        }
+    }
+
 #if UART_PRINTF_MODE
     printf("Bootloader is overwritten. Reset\r\n");
 #endif
